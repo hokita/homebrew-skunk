b829ed43125dc3709c19ac24ff1f916f7ba54d84
hokita <hideee.0202@gmail.com>
2024-07-15 17:45:23 +0900

    Upgrade turtle to a new version 0.0.3.

--- Formula/turtle.rb
+++ Formula/turtle.rb
@@ -1,8 +1,8 @@
 class Turtle < Formula
   desc 'Try a slow life like turtle.'
   homepage 'https://github.com/hokita/turtle'
-  url 'https://github.com/hokita/turtle/archive/0.0.2.tar.gz'
-  sha256 '062f0dcafa623b8c186ee42dd4cfd07a2ca888795b70864f977224f4c817dc79'
+  url 'https://github.com/hokita/turtle/archive/0.0.3.tar.gz'
+  sha256 '3bab070caac1a2c41d5830b8149dc3f67cbaa464993275a9602f1e1ecd319495'
   license 'MIT'
 
   depends_on 'go' => :build
